b80fd5fae0e0f0af1ad70c447f8f15642c2ef3bf
iprijic <iprijic21@gmail.com>
2023-03-06 13:01:27 +0100

    Implemented HTTP Delete method for existing person(s).

--- complete/src/main/java/rbacore/controller/CardController.java
+++ complete/src/main/java/rbacore/controller/CardController.java
@@ -1,14 +1,23 @@
 package rbacore.controller;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 import model.Card;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.BootstrapServiceRegistry;
+import org.hibernate.query.sqm.internal.SqmCriteriaNodeBuilder;
+import org.hibernate.query.sqm.tree.delete.SqmDeleteStatement;
+import org.hibernate.query.sqm.tree.from.SqmRoot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import rbacore.DataSourceBuilder;
@@ -52,5 +61,26 @@ public class CardController {
 
         return new ResponseEntity("ERROR: New person is not recorded into database properly.", HttpStatus.INTERNAL_SERVER_ERROR);
         //return "ERROR: New person is not recorded into database properly.";
+     }
+
+
+     @DeleteMapping(value = "/person")
+    public  ResponseEntity delete(@RequestParam(value="id", required=true) long id) {
+
+         Session session = sessionFactory.openSession();
+         session.getTransaction().begin();
+         SqmCriteriaNodeBuilder cb = (SqmCriteriaNodeBuilder) sessionFactory.openSession().getCriteriaBuilder();
+         Class<?> entityType = DataSourceBuilder.GetPersistType(sessionFactory,TypeFromModel());
+         SqmDeleteStatement delete = cb.createCriteriaDelete(entityType);
+         SqmRoot entity = (SqmRoot) delete.from(entityType);
+         delete.where(cb.equal(entity.get("id"), id));
+         int result = session.createMutationQuery(delete).executeUpdate();
+         session.getTransaction().commit();
+         if(result > 0) {
+             return new ResponseEntity("Person is deleted successfully.", HttpStatus.OK);
+         }
+         else {
+             return new ResponseEntity("Failed delete operation.This person does not exist.", HttpStatus.NOT_FOUND);
+         }
      }
 }
